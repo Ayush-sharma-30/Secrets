57aa5fe44fe2a4c158949e763f12e1cdb310f9f3
Ayush sharma <dsiayush30@gmail.com>
2023-08-22 20:07:13 +0530

    Added environment variables and gitignore file

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
+require('dotenv').config();
 const express  = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 var encrypt = require('mongoose-encryption');
+
 
 const app = express();
 
@@ -18,8 +20,7 @@ const userSchema = new mongoose.Schema({
   password: {type: String, required: true}
 });
 
-const secret = "thisisourlittlesecret";
-userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password']});
+userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password']});
 
 const User = new mongoose.model("User",userSchema);
 
